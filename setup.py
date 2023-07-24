577e78b055e99bef18062283be3972eeb06c240a
Qewertyy <99824993+Qewertyy@users.noreply.github.com>
2023-07-24 11:07:19 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,23 @@
+# Copyright 2023 Qewertyy, MIT License
+from setuptools import setup
+
+setup(
+    name="openapi",
+    version="0.0.1",
+    author="Qewertyy",
+    author_email="Qewertyy.irl@gmail.com",
+    description="The python package for api.qewertyy.me",
+    url="https://github.com/Qewertyy/Open-API",
+    packages=find_packages(exclude=[]),
+    python_requires=">=3.6",
+    install_requires=[
+        "requests",
+        "aiohttp",
+        "httpx[http2]",
+        "asyncio"
+    ],
+    keywords="Python, API, Bard, Google Bard, Large Language Model, Chatbot API, Google API, Chatbot, Image Generations, Latent Diffusion, State of Art",
+    classifiers=[
+        "API",
+    ]
+)
