d43bbc8726ab6b43f4071c3dd7ab56b84921d592
Qewertyy <99824993+Qewertyy@users.noreply.github.com>
2023-10-08 12:30:01 +0000

    fixes

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 # Copyright 2023 Qewertyy, MIT License
-from setuptools import setup
+from setuptools import setup,find_packages
 
 def get_long_description():
     with open("README.md", encoding="UTF-8") as f:
@@ -8,7 +8,7 @@ def get_long_description():
 
 setup(
     name="lexica-api",
-    version="1.3.6",
+    version="1.3.7",
     author="Qewertyy",
     author_email="Qewertyy.irl@gmail.com",
     description="The python package for api.qewertyy.me",
@@ -16,6 +16,7 @@ setup(
     python_requires=">=3.8",
     long_description=get_long_description(),
     long_description_content_type="text/markdown",
+    packages=find_packages(),
     install_requires=[
         "httpx[http2]",
         "asyncio"
